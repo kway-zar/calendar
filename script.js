cfe33c691df8b39c0335792c8ed251856c564168
kway-zar <137285693+kway-zar@users.noreply.github.com>
2024-02-26 20:55:00 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,65 @@
+monthDisplay = document.getElementById('p');
+dayList = document.getElementById("days");
+list_el = document.getElementsByTagName('li');
+
+const months = ["Jan", "Feb", "Mar","Apr","May","June","July", "Aug", "Sep","Oct", "Nov","Dec"]; 
+//need to implement the change month buttons
+
+const date = new Date();
+const day = date.getDate();
+const month = date.getMonth();
+console.log(month);
+
+monthDisplay.innerHTML = months[month];
+console.log(monthDisplay.innerHTML);
+
+
+
+switch(monthDisplay.innerHTML) {
+    case "Jan":
+    case "Mar":
+    case "May":
+    case "Jul":
+    case "Aug":
+    case "Oct":
+    case "Dec":
+
+        console.log(true);
+        
+        daysOftheMonth(31);
+        break;
+
+    case "Feb":
+        daysOftheMonth(29);
+        break;
+
+    case "Apr":
+    case "Jun":
+    case "Sep":
+    case "Nov":
+        daysOftheMonth(30);
+
+
+
+
+
+}
+
+function daysOftheMonth(j) {
+    for(let i = 1; i<= j; i++) {
+        list = document.createElement('li');
+        dayList.appendChild(list);
+
+        if(i == day) {
+           
+           list.outerHTML = '<li><div class="active"></div>'+ day + '</li>';
+           continue;
+        }
+        
+        textnode = document.createTextNode(i);
+        list.appendChild(textnode);
+        
+
+        
+   }
+}
